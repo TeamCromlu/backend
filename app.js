780c212c41c4d7a13f684159a12e8b1525210cf0
Joel Gonzales Tipismana <joelengt@gmail.com>
2015-10-30 17:42:42 -0500

    backen apico aniadido

--- app.js
+++ app.js
@@ -1,39 +1,105 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
-var nodemailer = require('nodemailer');
 
-// create reusable transporter object using SMTP transport
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'gmail.user@gmail.com',
-        pass: 'userpass'
-    }
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/', users);
+
+app.get('/500', function (req, res){
+  res.render('error_500')
 });
 
-// NB! No need to recreate the transporter object. You can use
-// the same transporter object for all e-mails
+// app.get('/preview', function (req, res){
+//   res.render('tem_mail')
+// });
 
-// setup e-mail data with unicode symbols
-var mailOptions = {
-    from: 'Fred Foo ✔ <foo@blurdybloop.com>', // sender address
-    to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world ✔', // plaintext body
-    html: '<b>Hello world ✔</b>' // html body
-};
+// app.get('/preview_0', function (req, res){
+//   res.render('mail_invitacion')
+// });
 
-// send mail with defined transport object
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
+// app.get('/preview_1', function (req, res){
+//   res.render('mail_1')
+// });
 
-});
+// app.get('/preview_2', function (req, res){
+//   res.render('mail_2')
+// });
 
-var port = 3000;
 
-app.listen(port, function (){
-    console.log('Puerto abierto en ', port);
+// app.get('/preview_3', function (req, res){
+//   res.render('mail_3')
+// });
+
+// app.get('/preview_4', function (req, res){
+//   res.render('mail_4')
+// });
+
+// app.get('/preview_5', function (req, res){
+//   res.render('mail_5')
+// });
+
+// app.get('/preview_6', function (req, res){
+//   res.render('mail_6')
+// });
+
+
+app.get('/send-comprar', function (req, res){
+  res.render('send_ok-comprar');
 })
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  res.render('error_404');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error_500', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error_500', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+
+module.exports = app;
